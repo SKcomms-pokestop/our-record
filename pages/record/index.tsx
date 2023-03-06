3811017191e2b6be2149a5eaa272a03a1d0bbc55
suksa <suksa_@naver.com>
2023-03-06 18:06:19 +0900

    녹음,,,

--- pages/record/index.tsx
+++ pages/record/index.tsx
@@ -1,93 +1,93 @@
-// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import React, { useEffect, useState } from 'react';
-// import { Button } from 'react-native';
-// import AudioRecorderPlayer from 'react-native-audio-recorder-player';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import axios from 'axios';
 
-// export default function Record() {
-//   const [recording, setRecording] = useState(false);
-//   const [audioFile, setAudioFile] = useState('');
-//   const [audioRecorderPlayer, setAudioRecorderPlayer] = useState(null);
+const recorder = new AudioRecorderPlayer();
 
-//   useEffect(() => {
-//     const initAudioRecorderPlayer = async () => {
-//       const result = await AudioRecorderPlayer.checkAuthorizationStatus();
-//       if (result === 'authorized') {
-//         setAudioRecorderPlayer(new AudioRecorderPlayer());
-//       }
-//     };
-//     initAudioRecorderPlayer();
-//   }, []);
+const Recorder = () => {
+  const [audioData, setAudioData] = useState<any>(null);
 
-//   const startRecording = async () => {
-//     const result = await audioRecorderPlayer.startRecorder();
-//     setRecording(true);
-//     console.log(result);
-//   };
+  const startRecord = async () => {
+    const result = await recorder.startRecorder();
 
-//   const stopRecording = async () => {
-//     const result = await audioRecorderPlayer.stopRecorder();
-//     setRecording(false);
-//     console.log(result);
-//   };
+    recorder.addRecordBackListener(e => {});
+    // console.log(result);
+  };
 
-//   const sendAudioFile = async () => {
-//     const formData = new FormData();
-//     formData.append('file', {
-//       uri: audioFile,
-//       type: 'audio/m4a',
-//       name: 'audio.m4a',
-//     });
+  const stopRecord = async () => {
+    const result = await recorder.stopRecorder();
 
-//     try {
-//       const response = await axios.post(
-//         'https://your-api-endpoint.com/upload-audio',
-//         formData,
-//         {
-//           headers: {
-//             'Content-Type': 'multipart/form-data',
-//           },
-//         },
-//       );
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
+    recorder.removeRecordBackListener();
+    setAudioData(result);
+    // console.log(result);
+  };
 
-//   return (
-//     <View style={styles.container}>
-//       <TouchableOpacity
-//         style={styles.button}
-//         onPress={recording ? stopRecording : startRecording}>
-//         <Text style={styles.buttonText}>
-//           {recording ? 'Stop Recording' : 'Start Recording'}
-//         </Text>
-//       </TouchableOpacity>
-//       {audioFile !== '' && (
-//         <TouchableOpacity style={styles.button} onPress={sendAudioFile}>
-//           <Text style={styles.buttonText}>Send Audio File</Text>
-//         </TouchableOpacity>
-//       )}
-//     </View>
-//   );
-// }
+  const sendAudio = async () => {
+    if (audioData) {
+      //   const formData = new FormData();
+      //   formData.append('audio', {
+      //     uri: audioData.path,
+      //     type: 'audio/mp4',
+      //     name: 'audio.mp4',
+      //   });
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   button: {
-//     backgroundColor: '#007AFF',
-//     borderRadius: 8,
-//     paddingVertical: 12,
-//     paddingHorizontal: 16,
-//     marginVertical: 8,
-//   },
-//   buttonText: {
-//     color: '#FFFFFF',
-//     fontSize: 16,
-//   },
-// });
+      //   try {
+      //     const response = await axios.post(
+      //       'https://your-api-url.com/upload',
+      //       formData,
+      //       {
+      //         headers: {
+      //           'Content-Type': 'multipart/form-data',
+      //         },
+      //       },
+      //     );
+      //     console.log(response);
+      //   } catch (error) {
+      //     console.log(error);
+      //   }
+      // CLOVA Speech API 정보
+      const CLIENT_ID = '5DEA700B7CABF06ACFA5';
+      const CLIENT_SECRET = 'ECD035ADE8C0AA26AB2402226533650204BC0C4E';
+      const LANGUAGE_CODE = 'ko-KR'; // 인식할 언어 코드
+      const URL = `https://clovaspeech-gw.ncloud.com/external/v1/4609/fec78d451acd9e86c125ddf2badf561475da1b8d288448a643e850487eb9ad17/recognizer/url`;
+      // 파일 정보
+      const formData = new FormData();
+      formData.append('data', audioData.path);
+
+      // Axios를 사용하여 CLOVA Speech API 요청
+      try {
+        const response = await axios.post(URL, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'X-NCP-APIGW-API-KEY-ID': CLIENT_ID,
+            'X-NCP-APIGW-API-KEY': CLIENT_SECRET,
+            'X-CLOVASPEECH-API-KEY': 'd0ba313a3af74d8798911ee6c51e4564',
+          },
+        });
+        console.log(response);
+        // 음성 인식 결과는 다음과 같이 확인할 수 있습니다.
+        const resultText = response.data.text;
+        console.log(resultText);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={startRecord}>
+        <Text>Start Recording</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={stopRecord}>
+        <Text>Stop Recording</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={sendAudio}>
+        <Text>Send Audio</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Recorder;
